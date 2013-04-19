68f01fb4336c3b796bb6a287cd19f17c6ce98fda
Raghu Betina <rbetina@gmail.com>
2013-04-19 11:05:02 -0500

    RCAV for showing the details of one picture.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,8 @@ class PicturesController < ApplicationController
   def list_of_pictures
     @pictures = Picture.all
   end
+
+  def picture_details
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rxngif::Application.routes.draw do
-  get("/pictures", { :controller => "Pictures", :action => "list_of_pictures" })
+  get "/pictures", :controller => "Pictures", :action => "list_of_pictures"
+
+  get "/pictures/:number", :controller => "Pictures", :action => "picture_details"
+
 end
