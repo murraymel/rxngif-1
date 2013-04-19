8d8d0900579f78ea05a20c6ddf0b4baa88f41bf6
Raghu Betina <rbetina@gmail.com>
2013-04-19 10:19:58 -0500

    Added instructions to migration file.

--- db/migrate/20130419151627_create_pictures.rb
+++ db/migrate/20130419151627_create_pictures.rb
@@ -1,5 +1,10 @@
 class CreatePictures < ActiveRecord::Migration
   def up
+    create_table :pictures do |t|
+      t.string :source
+      t.text :caption
+      t.boolean :favorite
+    end
   end
 
   def down
