44316358c857ecd3e6082d1fd5ae9897236ff87c
Raghu Betina <rbetina@gmail.com>
2013-04-22 10:43:05 -0500

    Added a route to handle form submission.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rxngif::Application.routes.draw do
   get "/pictures", :controller => "Pictures", :action => "list_of_pictures"
   get "/pictures/new", :controller => "Pictures", :action => "new_picture_form"
   get "/pictures/:number", :controller => "Pictures", :action => "picture_details"
+  get "/add_to_pictures", :controller => "Pictures", :action => "insert_picture"
 end
