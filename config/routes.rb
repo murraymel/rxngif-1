52edd7c625896abddd04ed5575f9ba598d800897
Raghu Betina <rbetina@gmail.com>
2013-04-22 12:30:36 -0500

    Re-organized routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rxngif::Application.routes.draw do
-  get "/pictures", :controller => "Pictures", :action => "list_of_pictures"
+  # CREATE
   get "/pictures/new", :controller => "Pictures", :action => "new_picture_form"
-  get "/pictures/:number", :controller => "Pictures", :action => "picture_details"
   get "/add_to_pictures", :controller => "Pictures", :action => "insert_picture"
+
+  # READ
+  get "/pictures", :controller => "Pictures", :action => "list_of_pictures"
+  get "/pictures/:number", :controller => "Pictures", :action => "picture_details"
+
+  # UPDATE
   get "/pictures/:number/edit", :controller => "Pictures", :action => "edit_picture_form"
   get "/update_picture/:number", :controller => "Pictures", :action => "update_picture"
 end
